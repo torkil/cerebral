b757739d91d039d241b222b9e6490d9fc59eccf6
andtan <jornandretangen@gmail.com>
2012-06-22 00:59:39 +0200

    constructor now adds a ViewsCollection subviews on the view in the constructor

--- test/specs/cerebral/mvc/View.js
+++ test/specs/cerebral/mvc/View.js
@@ -1,11 +1,38 @@
 define([
-  "cerebral/mvc/View"
-], function(View) {
-  
-  describe('cerebral/mvc/View', function() {
-    it('inherits from Backbone.View', function() {
-      expect(new View).to.be.a(Backbone.View)
+  "backbone",
+  "cerebral/mvc/View",
+  "cerebral/mvc/ViewCollection"
+], 
+function(Backbone, View, ViewCollection) {  
+  describe("cerebral/mvc/View", function() {
+    describe("constructor", function() {
+      it("instances should inherit from Backbone.View", function() {
+        expect(new View()).to.be.a(Backbone.View)
+      })
+      it("should set attribute subviews of type ViewCollection on the created instance", function() {
+        var vc = new View()
+        expect(vc.subviews).to.be.ok()
+        expect(vc.subviews).to.be.a(ViewCollection)
+      })
+      it('should call the supplied initialize function in the constructor', function() {
+        var ViewClass = View.extend({
+          initialize: function() { 
+            this.initializeed = true
+          }
+        })
+        var vc = new ViewClass()
+        expect(vc.initializeed).to.equal(true)
+      })
+      it("should set subview even if initialize is overidden", function() {
+        var ViewClass = View.extend({
+          initialize: function() { 
+            this.initializeed = true
+          }
+        })
+        var vc = new ViewClass()
+        expect(vc.subviews).to.be.ok()
+        expect(vc.subviews).to.be.a(ViewCollection)
+      })
     })
   })
-
 })
