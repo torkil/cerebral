a96b2df8f1e79e1b6142203af1e5cf2315e440ab
andtan <jornandretangen@gmail.com>
2012-06-22 13:24:41 +0200

    Formated to conform to idiomatic.js 2.A
    Beautiful Syntax

--- cerebral/mvc/View.js
+++ cerebral/mvc/View.js
@@ -7,14 +7,14 @@ function(Backbone, ViewCollection){
 
   var View = Backbone.View.extend({
     constructor: function() {
-      Backbone.View.prototype.constructor.apply(this, arguments)
+      Backbone.View.prototype.constructor.apply( this, arguments )
       this.subviews = new ViewCollection({
         superview: this
       })
-      if(typeof this.initialize === 'function')
+      if( typeof this.initialize === 'function' )
         this.initialize.apply(this, arguments)
     }
   })
-  window.View = View
+  
   return View
 })
--- cerebral/mvc/ViewCollection.js
+++ cerebral/mvc/ViewCollection.js
@@ -1,15 +1,15 @@
 define(
-"cerebral/mvc/ViewCollection",[
-  "cerebral/lib/EventEmitter",
-  "underscore",
-  "backbone",
+'cerebral/mvc/ViewCollection',[
+  'cerebral/lib/EventEmitter',
+  'underscore',
+  'backbone',
 ], 
-function(EventEmitter, _, Backbone) {
+function( EventEmitter, _, Backbone ) {
   
-  function ViewCollection(options) {
-    _.extend(this, options)
-    if(this.superview && !(this.superview instanceof Backbone.View))
-      throw new TypeError("ViewCollection received attribute superview not instance of Backbone.View")
+  function ViewCollection( options ) {
+    _.extend( this, options )
+    if( this.superview && !(this.superview instanceof Backbone.View) )
+      throw new TypeError( 'ViewCollection received attribute superview not instance of Backbone.View' )
   }
 
   ViewCollection.prototype = new EventEmitter()
@@ -18,67 +18,67 @@ function(EventEmitter, _, Backbone) {
 
   ViewCollection.prototype.length = 0
 
-  function attach(name, subview) {
-    if(typeof name !== "string")
-      throw new TypeError("parameter name not of type 'string'")
-    if(!(subview instanceof Backbone.View))
-      throw new TypeError('subview parameter not instance of Backbone.View')
-    if(this.superview) {
+  function attach( name, subview ) {
+    if( typeof name !== 'string' )
+      throw new TypeError( 'parameter name not of type "string"' )
+    if( !(subview instanceof Backbone.View) )
+      throw new TypeError( 'subview parameter not instance of Backbone.View' )
+    if( this.superview ) {
       subview.superview = this.superview
     }
-    this.views[name] = subview
-    this.trigger('attach', subview)
+    this.views[ name ] = subview
+    this.trigger( 'attach', subview )
     this.length++
   }
 
-  function attachArray(array) {
-    _.each(array, function(subview) {
-      attach.call(this, subview.cid, subview)
+  function attachArray( array ) {
+    _.each(array, function( subview ) {
+      attach.call( this, subview.cid, subview )
     }, this)
   }
 
-  function attachObject(obj) {
-    _.each(obj, function(name, subview) {
+  function attachObject( obj ) {
+    _.each(obj, function( name, subview ) {
       attach.call(this, name, subview)
     }, this)
   }
 
-  ViewCollection.prototype.attach = function(subviews) {
-    if(_.isArray(subviews))
-      return attachArray.call(this, subviews)
-    if(typeof subviews === 'object')
-      return attachObject.call(this, subviews)
-    throw new TypeError("parameter subviews not of correct type. Accepted: object{name: subview}, subviews, array of subviews")
+  ViewCollection.prototype.attach = function( subviews ) {
+    if( _.isArray(subviews) )
+      return attachArray.call( this, subviews )
+    if( typeof subviews === 'object' )
+      return attachObject.call( this, subviews )
+    throw new TypeError( 'parameter subviews not of correct type. Accepted: object{name: subview}, subviews, array of subviews' )
   }
 
-  function detachByInstance(view) {
+  function detachByInstance( view ) {
     var i, test
-    for (i = this.views.length - 1; i >= 0; i--) {
-      test = this.views[i]
-      if(view === test) {
-        delete this.views[i]
+    for ( i = this.views.length - 1; i >= 0; i-- ) {
+      test = this.views[ i ]
+      if( view === test ) {
+        delete this.views[ i ]
         this.length--
         //TODO: emit detach 
       }
     }
   }
 
-  function detachByCid(cid) {
-    if(this.views[cid]) {
-      delete this.views[cid]
+  function detachByCid( cid ) {
+    if( this.views[cid] ) {
+      delete this.views[ cid ]
       this.length--
       //TODO: emit detach
     } 
   }
 
-  ViewCollection.prototype.detach = function(cidOrView) {
-    if(typeof cidOrView === 'string') {
-      detachByCid.call(this, cidOrView)
+  ViewCollection.prototype.detach = function( cidOrView ) {
+    if( typeof cidOrView === 'string' ) {
+      detachByCid.call( this, cidOrView )
     }
-    if(typeof cidOrView === 'object') {
-      if(!(cidOrView instanceof Backbone.View))
-        throw new TypeError('subview parameter not instance of Backbone.View')
-      detachByInstance.call(this, cidOrView)
+    if( typeof cidOrView === 'object' ) {
+      if( !(cidOrView instanceof Backbone.View) )
+        throw new TypeError( 'subview parameter not instance of Backbone.View' )
+      detachByInstance.call( this, cidOrView )
     }
   }
 
