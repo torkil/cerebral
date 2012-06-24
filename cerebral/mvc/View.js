a3b774d491053978f0cfa4b54239d378a4d8cd4c
andtan <jornandretangen@gmail.com>
2012-06-24 19:56:30 +0200

    not setting superview on subviews(circular). New methods for managing event bindings and disposing of them

--- cerebral/mvc/View.js
+++ cerebral/mvc/View.js
@@ -8,13 +8,34 @@ function(Backbone, ViewCollection){
   var View = Backbone.View.extend({
     constructor: function() {
       Backbone.View.prototype.constructor.apply( this, arguments )
-      this.subviews = new ViewCollection({
-        superview: this
-      })
+      this.bindings = []
+      this.subviews = new ViewCollection()
       if( typeof this.initialize === 'function' )
         this.initialize.apply(this, arguments)
     }
   })
+
+  View.prototype.bindTo = function( model, event, callback ) {
+    model.bind( event, callback, this )
+    this.bindings.push({
+      model: model,
+      event: event,
+      callback: callback
+    })
+  }
+
+  View.prototype.unbindFromAll = function() {
+    _.each(this.bindings, function( binding ) {
+      binding.model.unbind( binding.event, binding.callback )
+    })
+    this.bindings = []
+  }
+
+  View.prototype.dispose = function() {
+    this.unbindFromAll()
+    this.unbind()
+    this.remove()
+  }
   
   return View
 })
