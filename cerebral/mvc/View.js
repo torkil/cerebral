943c7983e0a4fd6baf8af4f0ad0533d69cb630da
andtan <jornandretangen@gmail.com>
2012-06-25 21:30:51 +0200

    New methods for unbinding listeners

--- cerebral/mvc/View.js
+++ cerebral/mvc/View.js
@@ -1,15 +1,15 @@
 define(
 "cerebral/mvc/View",[
   "backbone",
-  "cerebral/mvc/ViewCollection"
+  "cerebral/mvc/SubviewCollection"
 ], 
-function(Backbone, ViewCollection){
+function(Backbone, SubviewCollection){
 
   var View = Backbone.View.extend({
     constructor: function() {
       Backbone.View.prototype.constructor.apply( this, arguments )
       this.bindings = []
-      this.subviews = new ViewCollection()
+      this.subviews = new SubviewCollection()
       if( typeof this.initialize === 'function' )
         this.initialize.apply(this, arguments)
     }
@@ -22,20 +22,63 @@ function(Backbone, ViewCollection){
       event: event,
       callback: callback
     })
+    return this
   }
 
-  View.prototype.unbindFromAll = function() {
+  function unbindSpecificCallback( obj, event, callback ) {
+    return _.filter(this.bindings, function( binding, index ) {
+      if( binding.obj === obj && binding.event === event && binding.callback === callback ) {
+        binding.obj.unbind( binding.event, binding.callback )
+      } else {
+        return true
+      }
+    }, this)
+  }
+
+  function unbindSpecificEvent( obj, event ) {
+    return _.filter(this.bindings, function( binding, index ) {
+      if( binding.obj === obj && binding.event === event ) {
+        binding.obj.unbind( binding.event, binding.callback )
+      } else {
+        return true
+      }
+    }, this) 
+  }
+    
+  function unbindAllOnObject( obj ) {
+    return _.filter(this.bindings, function( binding, index ) {
+      if( binding.obj === obj ) {
+        binding.obj.unbind( binding.event, binding.callback )
+      } else {
+        return true
+      }
+    }, this)
+  }
+    
+  View.prototype.unbindFrom = function( obj, event, callback ) {
+    if( arguments.length === 3 )
+      this.bindings = unbindSpecificCallback.apply( this, arguments )
+    if( arguments.length === 2 )
+      this.bindings = unbindSpecificEvent.apply( this, arguments )
+    if( arguments.length === 1 )
+      this.bindings = unbindAllOnObject.apply( this, arguments )
+    return this
+  }
+
+  View.prototype.unbindAll = function() {
     _.each(this.bindings, function( binding ) {
       binding.obj.unbind( binding.event, binding.callback )
     })
     this.bindings = []
+    return this
   }
 
   View.prototype.dispose = function() {
-    this.unbindFromAll()
+    this.removeAllListeners()
     this.unbind()
     this.remove()
     this.trigger('dispose')
+    return this
   }
   
   return View
