2d00ca6f911436159b45042197e3511cb19afe99
andtan <jornandretangen@gmail.com>
2012-06-26 14:36:35 +0200

    New dispose tests

--- test/specs/cerebral/mvc/View.js
+++ test/specs/cerebral/mvc/View.js
@@ -1,19 +1,29 @@
 define([
   "backbone",
   "cerebral/mvc/View",
-  "cerebral/mvc/SubviewCollection"
+  "cerebral/mvc/ViewCollection"
 ], 
-function(Backbone, View, SubviewCollection) {  
+function(Backbone, View, ViewCollection) {  
   describe("cerebral/mvc/View", function() {
     describe("constructor", function() {
+      it("should call View.prototype.constructor and initialize once", function() {
+        var ctorCalled = 0
+        var ViewClass = View.extend({
+          initialize: function() { 
+            ctorCalled++
+          }
+        })
+        var vc = new ViewClass()
+        expect(ctorCalled).to.equal(1)
+      })
       it("instances should inherit from Backbone.View", function() {
         expect( new View() ).to.be.a(Backbone.View)
         expect( new View().cid ).to.be.ok()
       })
-      it("should set attribute subviews of type SubviewCollection on the created instance", function() {
+      it("should set attribute subviews of type ViewCollection on the created instance", function() {
         var vc = new View()
         expect(vc.subviews).to.be.ok()
-        expect(vc.subviews).to.be.a(SubviewCollection)
+        expect(vc.subviews).to.be.a(ViewCollection)
       })
       it('should call the supplied initialize function in the constructor', function() {
         var ViewClass = View.extend({
@@ -32,7 +42,7 @@ function(Backbone, View, SubviewCollection) {
         })
         var vc = new ViewClass()
         expect(vc.subviews).to.be.ok()
-        expect(vc.subviews).to.be.a(SubviewCollection)
+        expect(vc.subviews).to.be.a(ViewCollection)
       })
     })
     describe("View.prototype.bindTo", function() {
@@ -150,6 +160,18 @@ function(Backbone, View, SubviewCollection) {
         obj.trigger("update")
         expect(nrOfChangeFired).to.equal(1)
         expect(nrOfUpdateFired).to.equal(1)
+      })
+    })
+    describe("View.prototype.dispose", function() {
+      it("should emit a dispose event, passing the view itself as parameter", function() {
+        var view = new View(),
+          disposedView = null
+        view.on("dispose", function(view) {
+          disposedView = view
+        })
+        expect(disposedView).not.to.be.ok()
+        view.dispose()
+        expect(disposedView).to.equal(view)
       })
     })
   })
