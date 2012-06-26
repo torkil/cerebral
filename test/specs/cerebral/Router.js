eedf621f3057981361b97b3ffd5f16e500544ffe
andtan <jornandretangen@gmail.com>
2012-06-26 14:36:09 +0200

    Started work on router

--- test/specs/cerebral/Router.js
+++ test/specs/cerebral/Router.js
@@ -1,46 +1,23 @@
-define("cerebral/Router",[
-  "backbone"
-], 
-function( Backbone ){
-
-  var Router = Backbone.Router.extend({
-    constructor: function() {
-      Backbone.Router.prototype.constructor.apply( this, arguments )
-      if( typeof this.initialize === 'function' )
-        this.initialize.apply(this, arguments)
-    }
+require([
+  "backbone",
+  "cerebral/Router"
+], function(Backbone, Router) {
+  
+  describe("cerebral/Router", function() {
+    describe("constructor", function() {
+      it("should return a new router that extends Backbone.Router", function() {
+        var router = new Router()
+        expect(router).to.be.a(Backbone.Router)
+      })
+    })
+    describe("Router.extracPath", function() {
+      it("should extract the path from the given url", function() {
+        expect(Router.extractPath("http://www.site.com/foo/bar")).to.equal("/foo/bar")
+        expect(Router.extractPath("https://www.site.com/foo/bar")).to.equal("/foo/bar")
+        expect(Router.extractPath("www.site.com/foo/bar")).to.equal("/foo/bar")
+        expect(Router.extractPath("site.com/foo/bar")).to.equal("/foo/bar")
+      })
+    })
   })
-    
-  Router.prototype.viewDelegate = function(view) {
-    view.$el
-      .find('a')
-      .live('click', _.bind(this.linkClick, this))
-  }
-
-  Router.prototype.extractPath = function(url) {
-    var regex
-    regex = /[\.no|1-9*]\/(.*)/
-    url = regex.exec(url)
-    return url[1] ? url[1] : ''
-  }
-
-  Router.prototype.linkClick = function(event) {
-    var url, path
-    url = event.currentTarget.href
-    if(!this.sameOrigin(url)) {
-      return null
-    }
-    event.preventDefault()
-    path = this.extractPath(url)
-    this.navigate(path, {trigger: true})
-    messenger.publish('track:url', '/'+path)
-  }
-
-  Router.prototype.sameOrigin = function(href) {
-    var port, origin
-    port = location.port ? ':' + location.port : ''
-    origin = location.protocol + '//' + location.hostname + port
-    return href.match(origin) ? 1 : 0
-  }
 
 })
