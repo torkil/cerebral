c6868b49dbc1158cc6c226c9a110b87029bf3331
andtan <jornandretangen@gmail.com>
2012-06-27 03:02:12 +0200

    cerebral/mvc/Model

---
+++ cerebral/mvc/Model.js
@@ -0,0 +1,21 @@
+
+/**
+  @class Base Model class, for data models
+  @exports cerebral/mvc/Model
+  @extends Backbone.Model
+  @requires [Backbone]
+*/
+define("cerebral/mvc/Model", [
+  "backbone"
+],
+function( Backbone ) {
+  
+  /**
+    Creates a new Model
+    @public
+    @constructor
+  */
+  var Model = Backbone.Model.extend({})
+
+  return Model
+})
