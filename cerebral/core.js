1dc1f5338da2944848cd1ecb8be9370666809e1d
andtan <jornandretangen@gmail.com>
2012-07-09 00:09:00 +0200

    Refactor and docs

--- cerebral/core.js
+++ cerebral/core.js
@@ -1,3 +1,9 @@
+
+/**
+  The core module, responsible for mediating between modules using the publish-subscribe pattern
+  @exports cerebral/core
+  @requires [underscore]
+*/
 define("cerebral/core",[
   "underscore"
 ], 
@@ -5,57 +11,89 @@ function( _ ){
   
   var channels, core
 
+  /**
+    Holds the callback listeners bound to fire when published to that specific channel 
+    @private
+    @type Object
+  */
   channels = {}
 
-  core = {
-    
-    subscribe: function( channel, callback, context ) {
-      if( typeof channel !== 'string' ) {
-        throw new TypeError( 'channel must be string' ) 
-      }
-      if( typeof callback !== 'function' ) {
-        throw new TypeError( 'callback must be function' ) 
-      }
-      if( !channels[channel] ) {
-        channels[ channel ] = [] 
-      }
-      channels[ channel ].push({
-        callback: callback,
-        context: context || {}
-      })
-    },
+  core = {}
+  
+  /**
+    Binds a callback to be called when published to the channel given
+    @public
+    @type Function
+    @param {String} channel The name of the channel to bind the callback to
+    @param {Function} callback The callback to fire
+    @param {Object} context The context of the callback. The callback will have this paramter as its this value
+    @returns {cerebral/core} self
+  */
+  core.subscribe = function( channel, callback, context ) {
+    if( typeof channel !== 'string' ) {
+      throw new TypeError( 'channel must be string' ) 
+    }
+    if( typeof callback !== 'function' ) {
+      throw new TypeError( 'callback must be function' ) 
+    }
+    if( !channels[channel] ) {
+      channels[ channel ] = [] 
+    }
+    channels[ channel ].push({
+      callback: callback,
+      context: context || {}
+    })
+    return this
+  }
 
-    unsubscribe: function( channel, callback ) {
-      var listeners, index, listener
-      if( typeof channel !== 'string' ) {
-        throw new TypeError( 'channel must be string' ) 
-      }
-      listeners = channels[ channel ]
-      if( !listeners ) {
-        return null
-      }
-      for( index = 0; index < listeners.length; index++ ) {
-        listener = listeners[ index ] 
-        if( listener.callback === callback ) {
-          listeners.splice( index, 1 )
-        }
-      }
-    },
-
-    publish: function( channel ) {
-      var listeners, args, index, listener
-      listeners = channels[ channel ]
-      if( !listeners || !listeners.length ) {
-        return null
-      }
-      args = [].splice.call( arguments, 1 )
-      for( index = 0; index < listeners.length; index++ ) {
-        listener = listeners[ index ] 
-        listener.callback.apply( listener.context, args )
+  /**
+    Unbind the callback from firing when published to the given channel
+    @public
+    @type Function
+    @param {String} channel The name of the channel to unbind the callback from
+    @param {Function} callback The callback to unbind
+    @returns {cerebral/core} self
+  */
+  core.unsubscribe = function( channel, callback ) {
+    var listeners, index, listener
+    if( typeof channel !== 'string' ) {
+      throw new TypeError( 'channel must be string' ) 
+    }
+    listeners = channels[ channel ]
+    if( !listeners ) {
+      return null
+    }
+    for( index = 0; index < listeners.length; index++ ) {
+      listener = listeners[ index ] 
+      if( listener.callback === callback ) {
+        listeners.splice( index, 1 )
       }
     }
-
+    return this
   }
+
+  /**
+    Publish to a channel, passing the arguments after channel to the callback
+    @public
+    @type Function
+    @param {String} channel The name of the channel to unbind the callback from
+    @param {Mixed[]} args The arguments after channel to be passed to callback
+    @returns {cerebral/core} self
+  */
+  core.publish = function( channel ) {
+    var listeners, args, index, listener
+    listeners = channels[ channel ]
+    if( !listeners || !listeners.length ) {
+      return null
+    }
+    args = [].splice.call( arguments, 1 )
+    for( index = 0; index < listeners.length; index++ ) {
+      listener = listeners[ index ] 
+      listener.callback.apply( listener.context, args )
+    }
+    return this
+  }
+
   
   return core
 })
