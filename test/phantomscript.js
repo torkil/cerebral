28c37355f7e9bae9f74dfb98298e0452aed11662
andtan <jornandretangen@gmail.com>
2012-07-28 18:20:01 +0200

    Phantomscript to run scripts in phantomjs

---
+++ test/phantomscript.js
@@ -0,0 +1,51 @@
+
+var page = new WebPage();
+
+// Route "console.log()" calls from within the Page context to the main Phantom context (i.e. current "this")
+page.onConsoleMessage = function(msg) {
+  console.log(msg);
+};
+
+function waitFor( test, then ) {
+  var intervall
+  intervall = setInterval(function() {
+    var data
+    data = test()
+    if( data ) {
+      clearInterval( intervall )
+      then( data )
+    }
+  }, 100)
+}
+
+page.open(phantom.args[0], function(status){
+  if (status !== "success") {
+    console.log("Unable to access network");
+    phantom.exit();
+  } else {
+    
+    waitFor(function() {
+      return page.evaluate(function(){
+        var stats = window.TESTSTATS
+        if( stats && stats.finnished )
+          return stats.data
+        else
+          return null
+      });
+    }, function( data ) {
+
+      console.log('\n\n');
+      console.log('passes: ', data.passes.length);
+      console.log('\n');
+      console.log('failures: ', data.failures.length);
+
+      if(data.failures.length) {
+        phantom.exit( 1 )
+      } else {
+        phantom.exit( 0 )
+      }
+
+    })
+
+  }
+});
