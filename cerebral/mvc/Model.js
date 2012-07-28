9895b76d68a1d962295e20b61a1593e851baf2d0
andtan <jornandretangen@gmail.com>
2012-07-29 01:12:45 +0200

    Methods for applying and defining getters and setters

--- cerebral/mvc/Model.js
+++ cerebral/mvc/Model.js
@@ -7,16 +7,92 @@
 */
 define(
 "cerebral/mvc/Model", [
+  "underscore",
   "cerebral/lib/Backbone"
 ],
-function( Backbone ) {
+function( _, Backbone ) {
   
   /**
     Creates a new Model
     @public
     @constructor
   */
-  var Model = Backbone.Model.extend({})
+  var Model = Backbone.Model.extend({
+    constructor: function() {
+      Backbone.Model.prototype.constructor.apply( this, arguments )
+
+      // Make sure defaults are not overwritte. Feels like a hack, plx fix.
+      _.extend(this.attributes, this.defaults )
+    }
+  })
+
+  /**
+    Defines a getter on the model instance. Wont be inherited
+    @public
+    @type Function
+    @param {String} key The attribute to fetch
+    @param {Function} Mutator The mutating function to apply to the value on get before returning
+  */
+  Model.prototype.defineGetter = function( key, mutator ) {
+    if( typeof key !== 'string' || typeof mutator !== 'function' ) {
+      throw new TypeError( 'Invalid params, key must be string and mutator function' )
+    }
+    this[ 'get:' + key ] = mutator
+  }
+
+  /**
+    Defines a setter on the model instance. Wont be inherited
+    @public
+    @type Function
+    @param {String} key The attribute to set
+    @param {Function} mutator The mutating function to apply to the value before setting it
+  */
+  Model.prototype.defineSetter = function( key, mutator ) {
+    if( typeof key !== 'string' || typeof mutator !== 'function' ) {
+      throw new TypeError( 'Invalid params, key must be string and mutator function' )
+    }
+    this[ 'set:' + key ] = mutator
+  }
+
+
+  /**
+    Get a value on the object. Applies getter if there is one.
+    @public
+    @type Function
+    @augments Backbone.Model.prototype.get
+  */
+  Model.prototype.get = function( attr ) {
+    var value, getter
+    value = this.attributes[ attr ]
+    getter = this[ 'get:' + attr ] || 0
+    if( getter ) {
+      return getter.call( this, value )
+    }
+    return value
+  }
+
+  /**
+    Get a value on the object. Applies setter if there is one.
+    @public
+    @type Function
+    @augments Backbone.Model.prototype.set
+  */
+  Model.prototype.set = function( key, value, options ) {
+    var attrs, attr, setter
+    if( _.isObject(key) ) {
+      attrs = key
+    } else {
+      attrs = {}
+      attrs[ key ] = value
+    }
+    for( attr in attrs ) {
+      setter = this[ 'set:' + attr ]
+      if( setter ) {
+        attrs[ attr ] = setter.call( this, attrs[attr] )
+      }
+    }
+    Backbone.Model.prototype.set.call( this, attrs, options )
+  }
 
   return Model
 })
