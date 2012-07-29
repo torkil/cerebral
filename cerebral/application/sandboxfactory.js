9fc59cde73d3e2b2d94a2b9876c1c0d1389f463a
andtan <jornandretangen@gmail.com>
2012-07-29 19:48:26 +0200

    new module sandboxfactory

---
+++ cerebral/application/sandboxfactory.js
@@ -0,0 +1,29 @@
+define(
+"cerebral/application/sandboxfactory",[
+  "underscore",
+  "jquery"
+], 
+function( underscore, $ ){
+  
+  function createSandbox( options ) {
+    var sandbox, element, jquery
+
+    sandbox = {}
+
+    element = $( options.el )
+    jquery = function( selector ) {
+      return $( selector, $(options.el) )
+    }
+    
+    sandbox.element = element
+    sandbox.$ = jquery
+
+    return sandbox
+  }
+
+  return {
+    create: function( options ) {
+      return createSandbox( options )
+    }
+  }
+})
