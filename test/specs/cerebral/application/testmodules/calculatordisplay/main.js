97c5c3ae757a4ed43a8f2175c973f457b3c42072
andtan <jornandretangen@gmail.com>
2012-07-29 19:48:15 +0200

    reporters for tests

--- test/specs/cerebral/application/testmodules/calculatordisplay/main.js
+++ test/specs/cerebral/application/testmodules/calculatordisplay/main.js
@@ -3,7 +3,20 @@ define([
   './views/Display'
 ], 
 function( DisplayModel, DisplayView ) {
-  return function() {
+  return function( sandbox ) {
     
+    if( TESTDATA.calculatordisplay.onMain )
+      TESTDATA.calculatordisplay.onMain()
+
+    if( TESTDATA.calculatordisplay.reportArguments ) {
+      TESTDATA.calculatordisplay.reportArguments({
+        sandbox: sandbox
+      })
+    }
+
+    if( TESTDATA.calculatordisplay.domAccess ) {
+      TESTDATA.calculatordisplay.domAccess( sandbox )
+    }
+      
   }
 })
