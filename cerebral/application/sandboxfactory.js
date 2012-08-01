4c4ecba7dd57d223abcaad3f616a9961d39048c3
andtan <jornandretangen@gmail.com>
2012-08-01 14:09:05 +0200

    Documentation of the sandboxfactory

--- cerebral/application/sandboxfactory.js
+++ cerebral/application/sandboxfactory.js
@@ -16,6 +16,11 @@ function( underscore, $ ){
 
   sandboxfactory = {}
 
+  /**
+    The properties to attach to the created sandbox objects prototype.
+    @private
+    @type Object
+  */
   properties = {
     $: function( selector ) {
       return $( selector, this.element )
@@ -23,6 +28,14 @@ function( underscore, $ ){
     element: '#sandbox'
   }
 
+  /**
+    Set a property in the properties object.
+    @public
+    @type Function
+    @param {String} name The name of the property
+    @param {Mixed} value The value of the property
+    @returns {cerebral/application/sandboxfactory} self
+  */
   sandboxfactory.defineProperty = function( name, value ) {
     if( typeof name !== 'string' ) {
       throw new TypeError( 'Name of property must be name' )
@@ -32,15 +45,32 @@ function( underscore, $ ){
     return this
   }
 
+  /**
+    Delete a property from the properties object.
+    @public
+    @type Function
+    @param {String} name The name of the property to remove
+    @returns {cerebral/application/sandboxfactory} self
+  */
   sandboxfactory.removeProperty = function( name ) {
     delete properties[ name ]
     return this
   }
 
+  /**
+    Create a new sandbox object with the properties on the properties object attached to its prototype
+    @public
+    @type Function
+    @param {Options} options The options for the sandbox
+    @param options.element The DOM element the sandbox has access to
+    @returns {Object} sandbox
+  */
   sandboxfactory.create = function( options ) {
-    var sandbox, element
+    var proto, sandbox, element
 
-    sandbox = Object.create( properties )
+    proto = _.clone( properties )
+
+    sandbox = Object.create( proto )
 
     element = options.element
     if( element ) {
