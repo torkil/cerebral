fe83a5e945ff615ff8cc771586c280f82745fcf7
andtan <jornandretangen@gmail.com>
2012-08-02 16:24:09 +0200

    destructible test modules

---
+++ test/specs/cerebral/application/testmodules/destructable/main.js
@@ -0,0 +1,25 @@
+define([
+  'sandbox'
+], 
+function( sandbox, InputModel, InputView, ButtonView ) {
+  
+  function main() {
+
+    if( TESTDATA.destructable.reportAlive ) {
+      TESTDATA.destructable.reportAlive( true )
+    }
+
+  }
+
+  function destruct( exit ) {
+
+    if( TESTDATA.destructable.onDestruct ) {
+      TESTDATA.destructable.onDestruct( true )
+    }
+
+    exit()
+  }
+
+  return { main: main, destruct: destruct }
+
+})
---
+++ test/specs/cerebral/application/testmodules/destructablefunctionmodule/main.js
@@ -0,0 +1,25 @@
+
+define([
+  'sandbox'
+], 
+function( sandbox, InputModel, InputView, ButtonView ) {
+  
+  function main() {
+    if( TESTDATA.destructablefunctionmodule.onMain ) {
+      TESTDATA.destructablefunctionmodule.onMain( true )
+    }
+
+  }
+
+  main.destruct = function( exit ) {
+
+    if( TESTDATA.destructablefunctionmodule.onDestruct ) {
+      TESTDATA.destructablefunctionmodule.onDestruct( true )
+    }
+
+    exit()
+  }
+
+  return main
+
+})
